0831a313dbff139beca5ff4450422ef98849fead
jgo-42 <jingmuio@gmail.com>
2023-01-28 08:50:59 +0900

    :art: Feat: add project structure file

---
+++ src/src_m/includes/utils.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 17:38:35 by jgo               #+#    #+#             */
+/*   Updated: 2023/01/25 17:50:42 by jgo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <sys/errno.h>
+#include <curses.h>
+#include <term.h>
+#include <termios.h>
+
+// 전부 다 사용할지는 모르겠지만 include 추가
+
+#endif
