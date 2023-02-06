d9d0e8bb3038ede205bfb7d9b76c66cc0c889f58
Sanghwa Lee <sanghwal@c5r7s6.42seoul.kr>
2023-02-06 21:35:54 +0900

    Draft: parser

--- src/src_m/parser/parser.c
+++ src/src_m/parser/parser.c
@@ -6,13 +6,31 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 16:46:50 by jgo               #+#    #+#             */
-/*   Updated: 2023/02/03 17:14:41 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/02/06 21:17:41 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    parser()
-{
-    tokenize();
+#include "minishell.h"
+#include "parser.h"
+#include "utils.h"
+#include "defines.h"
 
-    // tree 작업    
+t_tree	*parser(void)
+{
+	t_tree	*tree;
+	t_list	*tk_list;
+
+	tk_list = tokenize(tk_list);
+	// 토큰리스트 를 순회하면서 bnf 확인하기
+	// 토큰종류 (pipe, rdr, s_cmd)
+	// tree노드 종류 (pipe, CMD(rdr, s_cmd))
+	// '|'(root)노드를 먼저 하나 넣어놓고, \
+		CMD노드에 리다이렉션과 s_cmd넣어주기
+	// 파이프 확인
+	// 리다이렉션, 파일유무 확인
+		// 리다이렉션 뒤에는 file이 와야한다.
+	// 명령어, 인자 확인
+		// 리다이렉션과 file 제외한 첫번째 word가 cmd_name이 된다. 나머지는 cmd_args로 한다.
+	free_tk_list(&tk_list);
+	return (tree);
 }
--- src/src_m/utils/input.c
+++ src/src_m/utils/input.c
@@ -6,7 +6,7 @@
 /*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 16:06:38 by jgo               #+#    #+#             */
-/*   Updated: 2023/02/06 14:53:05 by sanghwal         ###   ########seoul.kr  */
+/*   Updated: 2023/02/06 20:37:08 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,12 @@
 void	prompt(void)
 {
 	char	*line;
-	t_list	*tk_list;
+	t_tree	*tree;
 
 	line = readline(MY_PROMPT);
 	if (!line || ft_strncmp(line, "exit", 5) == 0)
 		exit(EXIT_SUCCESS);
-	tk_list = tokenize(line);
-	// parser();
-	free_tk_list(&tk_list);
+	tree = parser();
 	// executor();
 	// printf("%s \n", line);
 	free(line);
