48c3acba09108efa2872435acfddad73ca268120
jgo-42 <jingmuio@gmail.com>
2023-02-18 21:10:20 +0900

    Temp: quote_removal

--- src/src_m/expander/expander_utils.c
+++ src/src_m/expander/expander_utils.c
@@ -1,33 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   input.c                                            :+:      :+:    :+:   */
+/*   expander_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/28 16:06:38 by jgo               #+#    #+#             */
-/*   Updated: 2023/02/18 20:37:55 by jgo              ###   ########.fr       */
+/*   Created: 2023/02/18 20:44:35 by jgo               #+#    #+#             */
+/*   Updated: 2023/02/18 20:49:56 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "defines.h"
-#include "utils.h"
-#include "expander.h"
-
-void    prompt()
-{
-	char *line;
-	char *tmp;
-
-	line = readline(MY_PROMPT);
-	if (!line || ft_strncmp(line, "exit", 5) == 0)
-		exit(EXIT_SUCCESS);
-	// parser();
-	// executor();
-	tmp = shell_param_expand(line);
-	tmp = quote_removal(tmp);
-	printf("%s\n", tmp);
-	free(line);
-	prompt();
-}
--- src/src_m/expander/quote_removal.c
+++ src/src_m/expander/quote_removal.c
@@ -6,16 +6,53 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 18:42:45 by jgo               #+#    #+#             */
-/*   Updated: 2023/02/08 20:59:20 by jgo              ###   ########.fr       */
+/*   Updated: 2023/02/18 21:05:31 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "defines.h"
+#include "data_structure.h"
 
+t_bool    is_same_quote(t_bool quote, char c, char *tmp)
+{
+    if (*tmp == c)
+        return (quote);
+    else
+    {
+        *tmp = c;
+        return (!quote);
+    }
+}
 
-// ft_strtrim
-// void    quote_removal(char *str)
-// {
-    
-// }
+int cal_remove_len(char *str)
+{
+    t_bool quote;
+    char tmp;
+    int len;
+    int i;
+
+    len = 0;
+    i = -1;
+    quote = FT_FALSE;
+    tmp = 0;
+    while(str[++i])
+    {
+        if (str[i] == S_QUOTE || str[i] == D_QUOTE)
+            quote = is_same_quote(quote, str[i], &tmp);
+        else
+        {
+            if (quote)
+                len++;
+        }
+    }
+    return (len);
+}
+
+char *quote_removal(char *str)
+{
+    const int len = cal_remove_len(str);
+
+    printf("quote_removal len : %d\n", len);
+    return (str);
+}
