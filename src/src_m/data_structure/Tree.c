3098dc8182da81f3a19accb86a2aa8bcccf0fa7b
Sanghwa Lee <sanghwal@c5r2s7.42seoul.kr>
2023-03-03 15:58:46 +0900

    Fix: pre_order_traversal

--- src/src_m/data_structure/Tree.c
+++ src/src_m/data_structure/Tree.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Tree.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
+/*   By: sanghwal <sanghwal@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 15:56:53 by jgo               #+#    #+#             */
-/*   Updated: 2023/02/07 18:01:04 by jgo              ###   ########.fr       */
+/*   Updated: 2023/03/03 15:57:46 by sanghwal         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,17 @@ void    insert(t_tree_node *node, t_tree_edge edge, t_tree_node *child)
 
 void    pre_order_traversal(t_tree_node *node, void(*f)(t_tree_node*))
 {
+	t_tree_node *left;
+	t_tree_node *right;
+
 	if (node == NULL)
 		return ;
+	left = node->left;
+	right = node->right;
 	f(node);
-	pre_order_traversal(node->left, f);
-	pre_order_traversal(node->right, f);
-};
+	pre_order_traversal(left, f);
+	pre_order_traversal(right, f);
+}
 
 void	delete_node(t_tree_node *node)
 {
