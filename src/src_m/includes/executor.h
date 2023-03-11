19ca23e739431c4c3892f49a4f74fe3280a4f232
jgo-42 <jingmuio@gmail.com>
2023-03-11 12:51:48 +0900

    :art: Norm: includes

--- src/src_m/includes/executor.h
+++ src/src_m/includes/executor.h
@@ -6,27 +6,37 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 17:51:16 by jgo               #+#    #+#             */
-/*   Updated: 2023/03/03 21:57:42 by jgo              ###   ########.fr       */
+/*   Updated: 2023/03/11 10:17:57 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTOR_H
 # define EXECUTOR_H
 
-void    executor(t_tree *tree);
+void				executor(t_tree *tree);
 
 /* is_built_in_cmd.c */
 
 t_simple_cmd_type	is_built_in_cmd(char *cmd);
 
 /* $(TYPE)_executor.c */
-void    rdr_executor(t_tree_node *node, t_executor *execute);
-void	rdr_restore(t_executor *execute);
-void	pipe_executor(t_tree_node *node, t_executor *execute, t_sequence sequence);
-void	cmd_executor(t_tree_node *node, t_executor *execute, t_sequence sequence);
+void				rdr_executor(t_tree_node *node, t_executor *execute);
+void				rdr_restore(t_executor *execute);
+void				pipe_executor(\
+	t_tree_node *node, t_executor *execute, t_sequence sequence);
+void				cmd_executor(\
+	t_tree_node *node, t_executor *execute, t_sequence sequence);
 
 /* executor_utils.c */
-t_token_type	check_token_type(t_tree_node *node);
+t_token_type		check_token_type(t_tree_node *node);
+t_bool				is_built_in(t_tree_node *root);
+t_bool				is_single(t_tree_node *root);
+void				shlvl_control(char *proc_name);
+
+/* cmd_executor_utils.c */
+const char			**get_path_arr(void);
+char				*make_abs_path(char *cmd, const char **path_arr);
+const char			**make_envp_arr(t_tree_node *node);
 
 /* exit status */
 # define EXIT_COM_NOT_FOUND 127
@@ -36,6 +46,5 @@ t_token_type	check_token_type(t_tree_node *node);
 
 // rdr실행할 때 error 처리 
 # define ERR_TOO_LONG ": File name too long"
-
 
 #endif
--- src/src_m/includes/expander.h
+++ src/src_m/includes/expander.h
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 20:27:08 by jgo               #+#    #+#             */
-/*   Updated: 2023/03/10 21:11:40 by jgo              ###   ########.fr       */
+/*   Updated: 2023/03/11 12:25:09 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,36 @@
 # define EXPANDER_H
 
 /* expander.c */
-void    expander(t_tree *tree);
+void	expander(t_tree_node *node);
 
 /* expander_utils.c */
 
 /* shell_parameter_expansion.c */
-char *shell_param_expand(char *str);
+char	*shell_param_expand(char *str);
 
 /* quote_removal.c */
-char *quote_removal(char *str);
+char	*quote_removal(char *str);
 
 /* shell_parameter_expansion_utils.c */
 
 void	quote_control(const t_deque *deque, char c);
-t_bool is_shell_var(char c);
+t_bool	is_shell_var(char c);
+void	adjust_param_idx(char *str, int *i);
 
 /* here_doc_expansino.c */
-char *here_doc_expand(char *str);
+char	*here_doc_expand(char *str);
 
 /* expander_utis.c */
 void	double_dollar(char *dst, char *str, int *i, int *j);
-int	expand_and_dup(char *dst, char *key, int j);
-int	try_expand_and_cal_len(char *str, int i, int tmp);
-char    is_same_quote(char quote, char c, int *len, char initial);
+int		expand_and_dup(char *dst, char *key, int j);
+int		try_expand_and_cal_len(char *str, int i, int tmp);
+char	is_same_quote(char quote, char c, int *len, char initial);
+t_bool	is_in_quote(const char *str);
 
 /* ft_field_split.c */
-char	**ft_field_split(char const *s, char c);
+void	cmd_field_split(t_simple_cmd *cmd);
+t_list	*split_node(char *str, const int str_len);
+void	arg_field_split(\
+	t_simple_cmd *cmd, char *str, int cur_idx, const int origin_len);
 
 #endif
